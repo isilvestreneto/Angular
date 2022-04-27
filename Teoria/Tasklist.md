215be27ccfb5f20e2e068e71c94e9c2f0be4217f
Ivan Silvestre <isilvestreneto@gmail.com>
2022-04-27 11:12:23 -0300

    0427-1

---
+++ Teoria/Tasklist.md
@@ -0,0 +1,8 @@
+# Tasklist
+- [] Decorators
+- [] Orientação a Objetos
+- [] Interfaces
+- [] Classes e atributos
+- [] Modificadores (private e public)
+- [] Métodos (get e set)
+- [] Variáveis e métodos estáticos (modificador static)
---
+++ hello-typescript/main.ts
@@ -0,0 +1,25 @@
+var minhaVar = "minha variável";
+
+function minhaFunc(x, y) {
+  return x + y;
+}
+
+// ES 6 OU ES 2015
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+  return valor * 2;
+});
+
+numeros.map((valor) => valor * 2); // ES 2015
+
+class Matematica {
+  soma(x, y) {
+    return x + y;
+  }
+}
+
+var n1: any = "saskpo";
+n1 = 4;
