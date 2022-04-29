8d1248032137f7ff74e7b52d9426f70c53e77125
Ivan Silvestre <isilvestreneto@gmail.com>
2022-04-29 19:48:11 -0300

    criandoDiretiva

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 import { CursosModule } from './cursos/cursos.module';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 
 @NgModule({
-  declarations: [AppComponent, MeuPrimeiroComponent, MeuPrimeiro2Component],
+  declarations: [AppComponent, MeuPrimeiroComponent, MeuPrimeiro2Component, DiretivaNgifComponent],
   imports: [BrowserModule, CursosModule],
   providers: [CursosService],
   bootstrap: [AppComponent],
