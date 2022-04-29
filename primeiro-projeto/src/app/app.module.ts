ef7763fbfa9acce28d5027840de0d4b1d11389cb
Ivan Silvestre <isilvestreneto@gmail.com>
2022-04-29 20:55:58 -0300

    tests

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 import { CursosModule } from './cursos/cursos.module';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 @NgModule({
-  declarations: [AppComponent, MeuPrimeiroComponent, MeuPrimeiro2Component, DiretivaNgifComponent],
+  declarations: [AppComponent, MeuPrimeiro2Component, DiretivaNgifComponent],
   imports: [BrowserModule, CursosModule],
   providers: [CursosService],
   bootstrap: [AppComponent],
