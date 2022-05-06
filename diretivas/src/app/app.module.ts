b0682d5786b3be17cfcd98b16bd90a0cb1a252e1
Ivan Silvestre <isilvestreneto@gmail.com>
2022-05-06 16:35:16 -0300

    diretiva-ngcontent

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.componen
 import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
 import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
 import { OperadorElvisComponent } from './operador-elvis/operador-elvis.component';
+import { DiretivaNgcontentComponent } from './diretiva-ngcontent/diretiva-ngcontent.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { OperadorElvisComponent } from './operador-elvis/operador-elvis.componen
     DiretivaNgforComponent,
     DiretivaNgclassComponent,
     DiretivaNgstyleComponent,
-    OperadorElvisComponent
+    OperadorElvisComponent,
+    DiretivaNgcontentComponent
   ],
   imports: [
     BrowserModule,
